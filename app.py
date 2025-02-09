29243ffc6357b1f00e9d842256a20c23ef2b0f2e
moroDev <nicolas.moro.fagetti@gmail.com>
2025-02-09 00:08:37 -0300

    Reutilizing 'results.html' for inserting and finding docs.

--- app.py
+++ app.py
@@ -24,28 +24,9 @@ def insertNew(data):
         return None
     else:
         data['id'] = idValue
-        print(data)
         insertion = myCollection.insert_one(data)
-        print(insertion)
         result = myCollection.find_one(insertion.inserted_id)
-        print(result)
         return result
-
-def insertData(item):
-    idValue = item["id"]
-    anyDoc = myCollection.find_one( {"id" : idValue} )
-    
-    if (anyDoc != None):
-        print("El id del elemento insertado ya existe. Desea actualizarlo con los datos brindados? (Y/N)")
-        wannaUpdate = input()
-        if (wannaUpdate.capitalize() == "Y"):
-            updateDataKeyValue("id", idValue, item)
-        else:
-            print("Item no modificado. Datos del item:\n")
-            queryOneItem(anyDoc)
-    else:        
-        result = myCollection.insert_one(item)
-        print(result)
 
 # Trae solo el primer documento
 def readOnce():
@@ -55,43 +36,45 @@ def readOnce():
 # Trae todos los documentos
 def readAll():
     result = myCollection.find()
-    for item in result:
-        print(item)
+    return result
 
 # Busca todos los documentos filtrados por el criterio (expresado en los clave-valor del diccionario pasado como parametro)
-def queryData(dict):
-    query = dict
-    result = myCollection.find(query)
+def queryData(queryDict):
+    result = myCollection.find(queryDict)
     return result
 
 # Busca el primer documento que satisfaga el criterio (expresado en los clave-valor del diccionario pasado como parametro)
-def queryOneItem(dict):
-    query = dict
-    result = myCollection.find_one(query)
+def queryOneItem(queryDict):
+    result = myCollection.find_one(queryDict)
     return result
 
 # Busca por ObjectID    
 def findByObjectID(_id):
     query = {"_id" : ObjectId(_id)}
     result = myCollection.find_one(query)
-    print(result)
+    return result
     
 # Actualiza el documento con el ObjectID pasado por el 1er parametro, con los datos brindados en el diccionario pasado por el 2do parametro
 def updateDataObjID(id, newData):
     query = {"_id" : ObjectId(id)}
-    newValues = {"$set":newData}                # $set --> actualiza solo los campos indicados por los pares clave-valor colocados a la derecha --> {"key":"value", "key":"value", ...}
+    newValues = {"$set" : newData}                # $set --> actualiza solo los campos indicados por los pares clave-valor colocados a la derecha --> {"key":"value", "key":"value", ...}
     myCollection.update_one(query, newValues)
+    actualizado = queryOneItem(query)
+    return actualizado
     
 # Actualizar el documento cuya clave (1er parametro) posee el valor indicado (2do parametro), con los nuevos valores del diccionario pasado en el 3er parametro
 def updateDataKeyValue(key, value, newData):
     query = {key : value}
     newValues = {"$set" : newData}
     myCollection.update_one(query, newValues)
+    actualizado = queryOneItem(query)
+    return actualizado
 
 # Eliminar documentos    
 def deleteDataObjID(id):
     query = {"_id" : ObjectId(id)}
-    myCollection.delete_one(query)
+    borrado = myCollection.delete_one(query)
+    return borrado.raw_result
 
 ###################################################################################################
 
@@ -109,7 +92,11 @@ def search():
         id = request.form.get('id')
         item = queryOneItem( {"id":int(id)} )
         print(item)
-        return render_template( "results.html", item=item )
+        if item == None:
+            return render_template( "results.html", item=item, mensaje=3 )
+        else:
+            return render_template( "results.html", item=item)
+            return 
 
 # Ruta formulario de insercion de registros a la BBDD #
 @app.route("/agregar")
@@ -134,6 +121,10 @@ def agregando_item():
             return render_template("results.html", item=resultado, nuevo=formInput, mensaje=1)
     else:
         return redirect("/")
+    
+@app.route("/actualizar")
+def actualizar():
+    return render_template("actualizar.html")
 
 
 if __name__ == "__main__":
