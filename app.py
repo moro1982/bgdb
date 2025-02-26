18f03e28d5f0de1788f24a86e487aa39af16e318
moroDev <nicolas.moro.fagetti@gmail.com>
2025-02-26 18:34:40 -0300

    Query functions improved - CRUD complete

--- app.py
+++ app.py
@@ -6,54 +6,67 @@ app = Flask(__name__)
 
 ###################################################################################################
 
-## DATABASE MANAGER ##
+### DATABASE MANAGER ###
 
 myClient = pymongo.MongoClient("mongodb://localhost:27017")     # Conexion
 myDB = myClient["bgdb"]                                         # Base de datos
 myCollection = myDB["items"]                                    # Coleccion
 
-# Ingresa un nuevo documento a la coleccion
+## Ingresa un nuevo documento a la coleccion ##
 def insertNew(data):
-    index = myCollection.count_documents({})
-    idValue = index + 1
-    anyDoc = myCollection.find_one( {"id" : idValue} )
     
-    if anyDoc :
-        print("El id del elemento insertado ya existe.")
-        return None
-    else:
-        data['id'] = idValue
-        insertion = myCollection.insert_one(data)
-        result = myCollection.find_one(insertion.inserted_id)
-        return result
+    for k in data:
+        if k in {"minPlayers", "maxPlayers", "minAge"}:
+            data[k] = int(data[k])
+        if k == "priceDollars":
+            data[k] = float(data[k])
+    
+    insertion = myCollection.insert_one(data)
+    print("ID insertado: " + str(insertion.inserted_id) + str(type(insertion.inserted_id)))
+    result = myCollection.find_one(insertion.inserted_id)
+    return result
 
-# Trae solo el primer documento
+## Trae solo el primer documento ##
 def readOnce():
     result = myCollection.find_one()
-    print(result)
+    return result
 
-# Trae todos los documentos
+## Trae todos los documentos ##
 def readAll():
     result = myCollection.find()
     return result
 
-# Busca todos los documentos filtrados por el criterio (expresado en los clave-valor del diccionario pasado como parametro)
+## Busca todos los documentos filtrados por el criterio (expresado en los clave-valor del diccionario pasado como parametro) ##
 def queryData(queryDict):
-    result = myCollection.find(queryDict)
+    # Creo un array con los elementos k:v del dict "filtrado"
+    items = []
+    for k in queryDict:
+        if k == "_id":
+            items.append({ k : ObjectId(queryDict[k]) })
+        else:
+            items.append({ k : queryDict[k] })
+    result = myCollection.find({"$and": items})
     return result
 
-# Busca el primer documento que satisfaga el criterio (expresado en los clave-valor del diccionario pasado como parametro)
+## Busca el primer documento que satisfaga el criterio (expresado en los clave-valor del diccionario pasado como parametro) ##
 def queryOneItem(queryDict):
-    result = myCollection.find_one(queryDict)
+    # Creo un array con los elementos k:v del dict "filtrado"
+    items = []
+    for k in queryDict:
+        if k == "_id":
+            items.append({ k : ObjectId(queryDict[k]) })
+        else:
+            items.append({ k : queryDict[k] })
+    result = myCollection.find_one({"$and": items})
     return result
 
-# Busca por ObjectID    
+## Busca por ObjectID ##
 def findByObjectID(_id):
     query = {"_id" : ObjectId(_id)}
     result = myCollection.find_one(query)
     return result
     
-# Actualiza el documento con el ObjectID pasado por el 1er parametro, con los datos brindados en el diccionario pasado por el 2do parametro
+## Actualiza el documento con el ObjectID pasado por el 1er parametro, con los datos brindados en el diccionario pasado por el 2do parametro ##
 def updateDataObjID(id, newData):
     query = {"_id" : ObjectId(id)}
     newValues = {"$set" : newData}                # $set --> actualiza solo los campos indicados por los pares clave-valor colocados a la derecha --> {"key":"value", "key":"value", ...}
@@ -61,7 +74,7 @@ def updateDataObjID(id, newData):
     actualizado = queryOneItem(query)
     return actualizado
     
-# Actualizar el documento cuya clave (1er parametro) posee el valor indicado (2do parametro), con los nuevos valores del diccionario pasado en el 3er parametro
+## Actualizar el documento cuya clave (1er parametro) posee el valor indicado (2do parametro), con los nuevos valores del diccionario pasado en el 3er parametro ##
 def updateDataKeyValue(key, value, newData):
     query = {key : value}
     newValues = {"$set" : newData}
@@ -69,7 +82,7 @@ def updateDataKeyValue(key, value, newData):
     actualizado = queryOneItem(query)
     return actualizado
 
-# Eliminar documentos    
+## Eliminar documentos ##
 def deleteDataObjID(id):
     query = {"_id" : ObjectId(id)}
     borrado = myCollection.delete_one(query)
@@ -77,55 +90,124 @@ def deleteDataObjID(id):
 
 ###################################################################################################
 
-## RUTAS ##
+### FUNCIONES AUXILIARES ###
 
-# Ruta Home #
+## Funcion para quitar campos vacios y corregir tipos numericos ##
+def corregirDatosForm(datos):
+    # Filtro el el documento por comprension
+        filtrado = { k:v for k,v in datos.items() if v != '' }
+        # Modifico el tipo de dato de los campos numericos (string a int o float)
+        for k in filtrado:
+            if k in {"minPlayers", "maxPlayers", "minAge"}:
+                filtrado[k] = (int)(filtrado[k])
+            if k == "priceDollars":
+                filtrado[k] = (float)(filtrado[k])
+        return filtrado
+
+###################################################################################################
+
+### RUTAS ###
+
+## Ruta Home ##
 @app.route("/")
 def main():
     return render_template("index.html")
 
-# Ruta intermedia del metodo POST activado desde formulario del "index.html"
+## Ruta intermedia del metodo POST activado desde formulario del "index.html" ##
 @app.route("/search", methods=['GET', 'POST'])
 def search():    
-    if request.method == 'POST':
-        id = request.form.get('id')
-        item = queryOneItem( {"id":int(id)} )
-        print(item)
-        if item == None:
-            return render_template( "results.html", item=item, mensaje=3 )
+    if request.method == "POST":
+        
+        # Extraigo los datos del formulario a un diccionario
+        datos = request.form.to_dict()
+        # Aplico funcion para quitar campos vacios y corregir tipos numericos
+        filtrado = corregirDatosForm(datos)
+        # Quito y almaceno el campo "accion"
+        accion = filtrado.pop("accion")
+        
+        # "filtrado" contiene los parametros de busqueda para la funcion "queryData"
+        listaResultados = []
+        if filtrado.items().__len__() != 0:
+            # Si el "filtrado" posee elementos, lo pasamos como parametro de busqueda
+            listaResultados = queryData(filtrado).to_list()
+            print(listaResultados)
         else:
-            return render_template( "results.html", item=item)
-            return 
-
-# Ruta formulario de insercion de registros a la BBDD #
+            # Si el "filtrado" esta vacio, retornara todos los documentos
+            listaResultados = readAll().to_list()
+            print(listaResultados)
+            
+        # Contamos la cantidad de elementos encontrados
+        cantidad = len(listaResultados)
+        print("Cantidad de elementos: " + str(cantidad))
+        if cantidad == 0:
+            # Si no encuentro elementos, paso solo el mensaje de error a la vista
+            return render_template( "results.html", mensaje=4 )
+        else:
+            # Si encuentro 1 o mas elementos, lo paso a la vista correspondiente (y si corresponde, tambien el mensaje)
+            if accion == "Buscar":
+                return render_template( "results.html", items=listaResultados )
+            if accion == "Actualizar":
+                item = listaResultados.pop()
+                return render_template( "actualizar.html", item=item )
+            if accion == "Eliminar":
+                return render_template( "results.html", items=listaResultados, mensaje=5 )
+            
+        
+## Ruta formulario de insercion de registros a la BBDD ##
 @app.route("/agregar")
 def agregar():
     return render_template("agregar.html")
 
-# Ruta intermedia del metodo POST activado desde formulario de "agregar.html" #
+## Ruta intermedia del metodo POST activado desde formulario de "agregar.html" ##
 @app.route("/agregando_item", methods=['GET', 'POST'])
 def agregando_item():
     if request.method == 'POST':
         formInput = request.form
         formInput = formInput.to_dict()
-        resultado = queryOneItem( {'name' : formInput['name']} )
+        filtrado = corregirDatosForm(formInput)
+        resultado = queryData( {'name' : filtrado['name']} )
+        listaEncontrados = resultado.to_list()
+        cantidad = listaEncontrados.__len__()
         
-        if resultado == None:
+        if cantidad == 0:
             # Insertamos los datos del nuevo juego
             agregado = insertNew(formInput)
-            if agregado != None:
-                return render_template("results.html", item=agregado, mensaje=2)
+            listaAgregados = []
+            listaAgregados.append(agregado)
+            
+            if len(listaAgregados) > 0:
+                return render_template("results.html", items=listaAgregados, mensaje=2)
+            else:
+                return render_template("results.html", mensaje=7)
         else:
             # No insertamos nada y avisamos que ya existe
-            return render_template("results.html", item=resultado, nuevo=formInput, mensaje=1)
-        
+            return render_template("results.html", items=listaEncontrados, nuevo=formInput, mensaje=1)
     else:
         return redirect("/")
-    
+
+## Ruta formulario de actualizacion de registros de la BBDD ##
 @app.route("/actualizar")
 def actualizar():
     return render_template("actualizar.html")
 
+## Ruta intermedia del metodo POST activado desde formulario de "actualizar.html" ##
+@app.route("/actualizando_item/<objID>/", methods=['GET', 'POST'])
+def actualizando_item(objID):
+    if request.method == 'POST':
+        formInput = request.form
+        formInput = formInput.to_dict()
+        formInput = corregirDatosForm(formInput)        
+
+        actualizado = updateDataKeyValue("_id", ObjectId(objID), formInput)
+        return render_template("results.html", item=actualizado, mensaje=3)
+
+## Ruta intermedia del metodo POST activado desde la vista "results.html" con accion "Eliminar" ##
+@app.route("/eliminar/<objID>")
+def eliminar(objID):
+    print(objID)
+    eliminado = deleteDataObjID(objID)
+    print(eliminado)
+    return render_template("results.html", item=eliminado, mensaje=6)
 
 if __name__ == "__main__":
     app.run(debug=True)
@@ -166,39 +248,78 @@ if __name__ == "__main__":
 #                            fromCountry=fromCountry,
 #                            priceDollars=priceDollars
 #                            )
+#
+################################################################################################
 
-
-# item01 = {"id" : 1,
-#           "name" : "Saboteur",
+# item01 = {"name" : "Saboteur",
 #           "minPlayers" : 3,
 #           "maxPlayers" : 10,
 #           "minAge" : 8,
 #           "fromCountry" : "Alemania",
 #           "priceDollars" : 15.25
 #           }
-# item02 = {"id" : 2,
-#           "name" : "Kremlin",
+# item02 = {"name" : "Kremlin",
 #           "minPlayers" : 2,
 #           "maxPlayers" : 6,
 #           "minAge" : 12,
 #           "fromCountry" : "Switzerland",
 #           "priceDollars" : 55.0
 #           }
-# item03 = {"id" : 3,
-#           "name" : "Catan",
+# item03 = {"name" : "Catan",
 #           "minPlayers" : 3,
 #           "maxPlayers" : 4,
 #           "minAge" : 10,
 #           "fromCountry" : "Alemania",
 #           "priceDollars" : 25.0
 #           }
-# item04 = {"id" : 4,
-#           "name" : "Rummy",
+# item04 = {"name" : "Rummy",
 #           "minPlayers" : 2,
 #           "maxPlayers" : 4,
 #           "minAge" : 8,
 #           "fromCountry" : "Inglaterra",
 #           "priceDollars" : 20.50
+#           }
+# item05 = {"name": "Las cobras de Shanghai",
+#           "minPlayers": "2",
+#           "maxPlayers": "10",
+#           "minAge": "6",
+#           "fromCountry": "China",
+#           "priceDollars": "12"
+#           }
+# item06 = {"name": "Magic: The Gathering",
+#           "minPlayers": "2",
+#           "maxPlayers": "2",
+#           "minAge": "13",
+#           "fromCountry": "EEUU",
+#           "priceDollars": "19.99"
+#           }
+# item07 = {"name": " Vampire: The Eternal Struggle",
+#           "minPlayers": "2",
+#           "maxPlayers": "5",
+#           "minAge": "13",
+#           "fromCountry": "EEUU",
+#           "priceDollars": "23"
+#           }
+# item08 = {"name": "Backgammon",
+#           "minPlayers": "2",
+#           "maxPlayers": "2",
+#           "minAge": "8",
+#           "fromCountry": "Irak",
+#           "priceDollars": "24.45"
+#           }
+# item09 = {"name": "Go",
+#           "minPlayers": "2",
+#           "maxPlayers": "2",
+#           "minAge": "10",
+#           "fromCountry": "Japon",
+#           "priceDollars": "30"
+#           }
+# item10 = {"name": "Generala",
+#           "minPlayers": "2",
+#           "maxPlayers": "10",
+#           "minAge": "5",
+#           "fromCountry": "Argentina",
+#           "priceDollars": "9.99"
 #           }
 
 # insertData(item04)
